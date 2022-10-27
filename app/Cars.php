ea2a81a6ca1c904638184ba776c0c6d24d442ce5
vitaliy <zndev@ya.ru>
2022-10-27 22:59:51 +0600

    fix Cars model

--- app/Cars.php
+++ app/Cars.php
@@ -2,12 +2,23 @@
 
 namespace App;
 
+use Faker\Core\DateTime;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Cars
+ * @package App
+ *
+ * @property integer $id
+ * @property string $title
+ * @property integer $user_id
+ * @property DateTime $created_at
+ * @property DateTime $updated_at
+ */
 class Cars extends Model
 {
 
     protected $fillable = [
-        'user_id'
+        'title'
     ];
 }
