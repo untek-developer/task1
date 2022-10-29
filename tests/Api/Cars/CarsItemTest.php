93b2fc1a2a3fa0cab4d5450378c6d8f1227cad66
vitaliy <zndev@ya.ru>
2022-10-29 21:04:36 +0600

    create test for item

---
+++ tests/Api/Cars/CarsItemTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Api\Cars;
+
+use Tests\TestCase;
+
+class CarsItemTest extends TestCase
+{
+
+    public function testItem1()
+    {
+        $response = $this->get('/api/v1/cars/1', $this->authHeaders());
+        $response->assertStatus(200);
+        $response->assertJson([
+            "id" => 1,
+            "title" => "Hyundai Solaris",
+            "user_id" => null,
+        ]);
+    }
+
+    public function testItem2()
+    {
+        $response = $this->get('/api/v1/cars/2', $this->authHeaders());
+        $response->assertStatus(200);
+        $response->assertJson([
+            "id" => 2,
+            "title" => "Toyota Camry",
+            "user_id" => null,
+        ]);
+    }
+
+    public function testItem2222()
+    {
+        $response = $this->get('/api/v1/cars/2222', $this->authHeaders());
+        $response->assertStatus(404);
+    }
+}
